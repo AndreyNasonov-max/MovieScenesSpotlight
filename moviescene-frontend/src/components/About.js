099d452d0a4891cca1a483dd34e18c68f97acd7f
AndreyNasonov-max <andreynasonov025@gmasil.com>
2024-06-11 21:11:25 -0300

    New Website

--- moviescene-frontend/src/components/About.js
+++ moviescene-frontend/src/components/About.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import '../componentcss/About.css'; // Adjust the import path according to your structure
 
 function About() {
@@ -8,14 +7,6 @@ function About() {
     <div className="about-container">
       <h1 className="about-title">About Us</h1>
       <p className="about-content">This is the about page.</p>
-=======
-
-function About() {
-  return (
-    <div>
-      <h1>About Us</h1>
-      <p>This is the about page.</p>
->>>>>>> 04d2616a3ad109ac22b15c75d8ee9360346d05b4
     </div>
   );
 }
--- moviescene-frontend/src/components/Contact.js
+++ moviescene-frontend/src/components/Contact.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import '../componentcss/Contact.css'; // Adjust the import path according to your structure
 
 function Contact() {
@@ -8,14 +7,6 @@ function Contact() {
     <div className="contact-container">
       <h1 className="contact-title">Contact Us</h1>
       <p className="contact-content">This is the contact page.</p>
-=======
-
-function Contact() {
-  return (
-    <div>
-      <h1>Contact Us</h1>
-      <p>This is the contact page.</p>
->>>>>>> 04d2616a3ad109ac22b15c75d8ee9360346d05b4
     </div>
   );
 }
